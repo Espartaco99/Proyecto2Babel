726ec2539b5e33119005e4ed50af73559d0cac5a
Jesus <jperucha@ucm.es>
2018-03-12 13:02:41 +0100

    Creados Modulos de todas las secciones del menu

---
+++ practica-angular/src/app/competiciones/competiciones.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CompeticionesRoutingModule } from './competiciones-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CompeticionesRoutingModule
+  ],
+  declarations: []
+})
+export class CompeticionesModule { }
