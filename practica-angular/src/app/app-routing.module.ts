45fb1dfcde5401d5d6489186d0a7673aacb8d20c
Jesus <jperucha@ucm.es>
2018-03-12 12:57:00 +0100

    Añadido font awesome al css y creado un modulo inicio

--- practica-angular/src/app/app-routing.module.ts
+++ practica-angular/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
-  { path: 'inicio', component:  },
+  { path: '', pathMatch: 'full', redirectTo: 'path' },
+  /* { path: 'inicio', component: '' }, */
 ];
 
 @NgModule({
