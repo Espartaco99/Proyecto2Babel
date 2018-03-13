776e20c5f0f4ed98d1432071509b09d97d683c13
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-03-13 10:27:43 +0100

    enrutamiento completo, colocacion inicial de componentes about

--- practica-angular/src/app/about/about-routing.module.ts
+++ practica-angular/src/app/about/about-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about.component';
 import { AutoresComponent } from './autores/autores.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { MuestraDatosComponent } from './muestra-datos/muestra-datos.component';
 
-const routes: Routes = [{ path: '', component: AutoresComponent },
-/* { path: 'autores', component: AutoresComponent },
- { path: 'formulario', component: FormularioComponent },*/
- { path: '../autores', component: AutoresComponent },
- { path: '../formulario', component: FormularioComponent },
+const routes: Routes = [{ path: '', pathMatch: ' full', redirectTo: 'autores' },
+ { path: 'autores', component: AutoresComponent },
+ { path: 'formulario', component: FormularioComponent },
+  { path: 'muestra-datos', component: MuestraDatosComponent },
 ];
 
 @NgModule({
