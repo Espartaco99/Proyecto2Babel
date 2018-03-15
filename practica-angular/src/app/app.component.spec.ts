e497d17f86d10551f9a6ebfbad202abf713a67f7
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-03-15 10:16:15 +0100

    css de pagina inicio terminado

--- practica-angular/src/app/app.component.spec.ts
+++ practica-angular/src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {CoreModule} from './core/core.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bbl!');
+    expect(compiled.querySelector('h1').textContent).toContain('Salto Base');
   }));
 });
